49689c20396ca1cbf3194e16b027c5d8bdb93512
Sakshi107 <46996938+Sakshi107@users.noreply.github.com>
2021-04-02 01:28:00 +0530

    further changes to maps
    Signed-off-by: Sakshi107 <46996938+Sakshi107@users.noreply.github.com>

--- parking_locator/lib/main.dart
+++ parking_locator/lib/main.dart
@@ -4,6 +4,7 @@ import 'package:parking_locator/models/place.dart';
 import 'package:parking_locator/screens/search.dart';
 import 'package:parking_locator/services/geolocator_service.dart';
 import 'package:parking_locator/services/places_service.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
@@ -17,9 +18,13 @@ class MyApp extends StatelessWidget {
     return MultiProvider(
       providers: [
         FutureProvider(create: (context) => locatorService.getLocation()),
-        ProxyProvider<Position,Future<List<Place>>>( 
-          update: (context,position,places){
-            return (position !=null) ? placesService.getPlaces(position.latitude, position.longitude) :null;
+        FutureProvider(create: (context) {
+          ImageConfiguration configuration = createLocalImageConfiguration(context);
+          return BitmapDescriptor.fromAssetImage(configuration, 'assets/images/parking-icon.png');
+        }),
+        ProxyProvider2<Position,BitmapDescriptor,Future<List<Place>>>( 
+          update: (context,position,icon,places){
+            return (position !=null) ? placesService.getPlaces(position.latitude, position.longitude,icon) :null;
           },
         )
       ],
--- parking_locator/lib/services/geolocator_service.dart
+++ parking_locator/lib/services/geolocator_service.dart
@@ -3,7 +3,12 @@ import 'package:geolocator/geolocator.dart';
 class GeoLocatorService{
 
   Future<Position> getLocation() async {
-    var geolocator = Geolocator();
+    
     return await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
   }
+
+  Future<double> getDistance(double startLatitude, double startLongitude, double endLatitude, double endLongitude) async {
+   return Geolocator.distanceBetween(startLatitude, startLongitude, endLatitude, endLongitude);
+  }
 }
+
