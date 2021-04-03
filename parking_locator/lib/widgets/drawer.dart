9a9dab626f4cf5ae94b229adb8cc44cd92bd2205
hiral72 <46996903+hiral72@users.noreply.github.com>
2021-04-03 19:22:13 +0530

    added drawer

--- parking_locator/lib/widgets/drawer.dart
+++ parking_locator/lib/widgets/drawer.dart
@@ -18,14 +18,14 @@ class NavDrawer extends StatelessWidget {
                 child: Column(
                   children: [
                     Text(
-                      'PARK APP',
+                      'BOOK PARK',
                       style: TextStyle(color: headingColor, fontSize: 28),
                     ),
                     CircleAvatar(
                       maxRadius:35,
                       backgroundColor: kMainColor,
                       child: Text(
-                        "MAP",
+                        "M",
                         style: TextStyle(color:headingColor,fontSize: 40.0),
                       ),
                     ),
@@ -37,36 +37,14 @@ class NavDrawer extends StatelessWidget {
             title: Text('Add Spot'),
             onTap: () => {Navigator.of(context).pushReplacementNamed('/add_spot')},
           ),
-           Divider(color: kMainColor),
-           ListTile(
-            title: Text('Add item '),
-            onTap: () => {Navigator.of(context).pushReplacementNamed('/add_item')},
-          ),
+        
            Divider(color: kMainColor),
           ListTile(
-            title: Text('Orders'),
-            onTap: () => {Navigator.of(context).pushReplacementNamed('/my_orders')},
-          ),
-           Divider(color: kMainColor),
-          ListTile(
-            title: Text('My Account'),
-            onTap: () => {Navigator.of(context).pushReplacementNamed('/my_account')},
-          ),
-           Divider(color: kMainColor),
-          ListTile(
-            title: Text('QC Data'),
-            onTap: () => {Navigator.of(context).pushReplacementNamed('/oc_data')},
-          ),
-           Divider(color: kMainColor),
-          ListTile(
-            title: Text('My Reviews'),
-            onTap: () => {Navigator.of(context).pushReplacementNamed('/my_reviews')},
+            title: Text('My Parking slots'),
+            onTap: () => {Navigator.of(context).pushReplacementNamed('/my_parking')},
           ),
           Divider(color: kMainColor),
-          ListTile(
-            title: Text('Contact Services'),
-            onTap: () => {Navigator.of(context).pushReplacementNamed('/contact_service')},
-          ),
+          
         ],
       ),
     ),
